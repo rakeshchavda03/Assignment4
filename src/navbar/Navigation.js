f8c768374a22d4d986d2186825abc3a9c9be5e45
chavada Rakesh <chavadarakesh672@gmail.com>
2023-03-10 13:18:56 +0530

    commited all

---
+++ src/navbar/Navigation.js
@@ -0,0 +1,34 @@
+// import React from "react";
+// import logo from '../image/logo.png';
+// import '../style/Navigation.css'
+// import userLogo from '../image/userlogo.png'
+// function Navbar() {
+//     return (
+//         <nav className="navbar navbar-expand-lg navbar-light bg-black">
+//             <div className="nav">
+//             <a className="navbar-brand" href="#">
+//                 <img src={logo}
+//                     width="30"
+//                     height="30"
+//                     alt=""/>
+//             </a>
+//             </div>
+//             <div className=" nav">
+//                 <input type="search" placeholder="Search" className="me-2" aria-label="Search"/>
+//                 <button className="btn btn-success" variant="outline-success">Search</button>
+//             </div>
+//             <div className="nav" >
+//             <a className="navbar-brand" href="#">
+//                 <img src={userLogo}
+//                     width="40"
+//                     height="40"
+//                     alt=""/>
+//             </a>
+//             </div>
+
+
+
+//         </nav>
+//     )
+// }
+// export default Navbar;
